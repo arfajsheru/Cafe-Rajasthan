51b48c29ae9db5390882a4fbbfbef4e3630e0bd4
arfajsheru <arfatsheru74@gmail.com>
2025-03-13 16:16:11 +0530

    listfood and remove food api working in frontend

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const port = 4000;
 // middleware
 app.use(express.json());
 app.use(cors({
-  origin: ["http://192.168.0.121"],  // Laptop ka IP
+  origin: ["http://192.168.0.122"],  // Laptop ka IP
   credentials: true
 }));
 
